41cfdb6938a37b4ab347bbf6e208cf70a56bbf53
poudel-bibek <serpent.03.meshes@icloud.com>
2024-04-12 09:35:46 -0400

    roadmap

---
+++ ECGEncoder.py
@@ -0,0 +1,13 @@
+
+class ECGEncoder():
+    def __init__(self, ):
+        pass
+    
+    def load_model(self, ):
+        pass 
+
+    def encode(self, ):
+        """
+        Make inference on the input ECG signal and return the encoded representation
+        """
+        pass
